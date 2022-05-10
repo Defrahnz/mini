946d96bd2883c10166952be35e25a577e887906e
Defrahnz <carlos.ibarra@maindsoft.net>
2022-05-10 17:04:45 -0500

    Se modifico el routing para dirigir las paginas y la navbar para mostrar un menu de acorde a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './about/about.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'heroes',component:HeroesComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
